ea2780abd29fe272a37e26265c8b5f509fde14bc
Hekter <dustin.sollick@gmail.com>
2015-01-01 05:48:25 -0800

    Reworked formats overall layout.

--- formats.py
+++ formats.py
@@ -1,28 +1,34 @@
 class Privmsg:
-    def __init__(self):
-        pass
+    def __init__(self, msg):
+        self.name = 'PRIVMSG'
+        self.msg = msg
+        self.isCommand = False
 
 
 class Ping:
-    def __init__(self):
-        pass
+    def __init__(self, msg):
+        self.name = 'PING'
+        self.msg = msg
+        self.isCommand = True
+    def do(self, connection):
+        connection.ircsock.send(str.encode("PONG " + self.msg[1] + "\r\n"))
 
 
-class Join:
-    def __init__(self):
-        pass
-
-
-class Part:
-    def __init__(self):
-        pass
+# class Join:
+#     def __init__(self, msg):
+#         pass
+#
+#
+# class Part:
+#     def __init__(self, msg):
+#         pass
 
 
 class code353:
-    def __init__(self):
-        pass
+    def __init__(self, msg):
+        self.name = 'Code353'
 
 
 class code366:
-    def __init__(self):
-        pass
+    def __init__(self, msg):
+        self.name = 'Code366'
