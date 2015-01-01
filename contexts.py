595d43f986dd7d900da3ea338c468e5662b1d23d
Hekter <dustin.sollick@gmail.com>
2015-01-01 05:26:42 -0800

    Simplified contexts.IRCContext.

--- contexts.py
+++ contexts.py
@@ -1,18 +1,5 @@
 class IRCContext:
-    def __init__(self, sockconn, homedir):
-        self.sockconn = sockconn
+    def __init__(self, ircsock, comchar, homedir):
+        self.ircsock = ircsock
+        self.comchar = comchar
         self.homedir = homedir
-        self.connected_chans = []
-
-    def getType(self, msg):
-        msgType = msg[1]
-
-
-    # def argGrabber(self, msg, argNum):
-    #     argResults = []
-    #     for x in range(1, argNum + 1):
-    #         try:
-    #             argResults.append(msg[3 + x])
-    #         except IndexError:
-    #             argResults.append(None)
-    #     return argResults
