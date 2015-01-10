e404be8586e46c864c35b65d3dff9e8b651b336b
Hekter <dustin.sollick@gmail.com>
2015-01-10 08:10:19 -0800

    Attempted to jam receiver() partial-message-processing functionality into here. It isn't pretty and doesn't work :D

--- commands/call.py
+++ commands/call.py
@@ -5,8 +5,14 @@ import utils
 import debugtools as debug
 
 def run(connection, privmsg):
+    connection.send_msg(privmsg.chan, "No.")
+
+def NOPE(connection, privmsg):
     # Empty dictionary for nicks to be added to later.
     nick_dict = {}
+
+    # partialmessage is set to blank for the moment as it's only used lower.
+    partialmessage = ""
     # calltext (what we're going to yell at everyone after we ping them) is everything after the @call!
     calltext = privmsg.post_command_text
     debug.echo(connection.debugmode, calltext, "calltext inside run() in call.py")
@@ -30,34 +36,45 @@ def run(connection, privmsg):
         # print(ircmsg)
         debug.echo(connection.debugmode, ircmsg, "ircmsg")
 
-        # Then we get and assign a message class to the message! Wee.
-        msg = utils.getMsgClass(ircmsg)
-        debug.echo(connection.debugmode, msg, "msg object after coming back from utils.getMsgClass")
+        while True:
+            ircmsg = partialmessage + ircmsg
+            partition = ircmsg.partition("\r\n")
+            debug.echo(connection.debugmode, str(partition), "partition")
+            if partition[1] == "":
+                partialmessage = partition[0]
+                break
+            else:
+                # Then we get and assign a message class to the message! Wee.
+                msg = utils.getMsgClass(partition[0])
+                debug.echo(connection.debugmode, msg, "msg object after coming back from utils.getMsgClass")
 
-        # If utils.getMsgClass() returns None, it means it's an unsupported/unparsed message type, so we
-        #     should loop back to the start.
-        if msg is None:
-            pass
-        elif msg.name == "Code353":
-            nicks = msg.get_nicks()
-            debug.echo(connection.debugmode, nicks, "nicks inside elif code353")
-            nick_dict[nicklist_count] = nicks
-            # Increment counter by 1.
-            nicklist_count += 1
-        elif msg.name == "PRIVMSG":
-            connection.send_msg(privmsg.get_chan(), "I'm busy at the moment with another command! Try again in a jiffy!")
-        elif msg.name == "Code366":
-            for x in list(nick_dict.values()):
-                try:
-                    connection.send_msg(privmsg.chan, x)
-                except OSError:
-                    raise
-            try:
-                connection.send_msg(privmsg.chan, calltext)
-            except OSError:
-                raise
-            break
-        else:
-            pass
+                # If utils.getMsgClass() returns None, it means it's an unsupported/unparsed message type, so we
+                #     should loop back to the start.
+                if msg is None:
+                    pass
+                elif msg.name == "Code353":
+                    nicks = msg.get_nicks()
+                    debug.echo(connection.debugmode, nicks, "nicks inside elif code353")
+                    nick_dict[nicklist_count] = nicks
+                    # Increment counter by 1.
+                    nicklist_count += 1
+                elif msg.name == "PRIVMSG":
+                    connection.send_msg(privmsg.get_chan(), "I'm busy at the moment with another command! Try again in a jiffy!")
+                    break
+                elif msg.name == "Code366":
+                    for x in list(nick_dict.values()):
+                        try:
+                            connection.send_msg(privmsg.chan, x)
+                        except OSError:
+                            raise
+                    try:
+                        connection.send_msg(privmsg.chan, calltext)
+                    except OSError:
+                        raise
+                    break
+                else:
+                    pass
+            ircmsg = partition[2]
+        break
 
     return
